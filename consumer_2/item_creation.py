8f606d26b00a15ca820d6781d5012dbfd26971d4
Lakshmeesh Bhat <sherlywatson44@gmail.com>
2024-04-13 23:33:58 +0530

    added queue to send current items to producer

--- consumer_2/item_creation.py
+++ consumer_2/item_creation.py
@@ -18,6 +18,7 @@ def connect_to_rabbitmq():
 			connection=pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
 			channel=connection.channel()
 			channel.queue_declare('item_creation_queue')
+			channel.queue_declare('get_items_queue')
 			print("Connected to rabbitmq")
 			return connection, channel
 		except pika.exceptions.AMQPConnectionError:
@@ -26,7 +27,6 @@ def connect_to_rabbitmq():
 
 
 connection, channel=connect_to_rabbitmq()
-
 def callback(ch, method, properties, body):
 	item_data=eval(body)
 	print(f"Received item creation request: {item_data}")
@@ -34,9 +34,24 @@ def callback(ch, method, properties, body):
 	values=(item_data["name"], item_data["description"], item_data["price"], item_data["quantity"])
 	cursor.execute(query, values)
 	db.commit()
+	itemlist=[]
+	query="select * from items"
+	cursor.execute(query)
+	items=cursor.fetchall()
+	for item in items:
+		itemlist.append({
+			'id': item[0],
+			'name': item[1], 
+			'description': item[2],
+			'price': float(item[3]), 
+			'quantity': item[4]
+		})
+	print(itemlist)
+	channel.basic_publish(exchange='', routing_key="get_items_queue", body=str(itemlist))
+	channel.basic_ack(delivery_tag=method.delivery_tag)
 	print("Item created successfully")
 
-channel.basic_consume(queue="item_creation_queue", on_message_callback=callback, auto_ack=True)
+channel.basic_consume(queue="item_creation_queue", on_message_callback=callback, auto_ack=False)
 print("Waiting for item creation requests")
 channel.start_consuming()
 
