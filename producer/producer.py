fd4247b76dea5f65dd38a84663b83608b7c358ca
Lakshmeesh Bhat <sherlywatson44@gmail.com>
2024-04-13 23:36:28 +0530

    item creation fetches current items and renders to user

--- producer/producer.py
+++ producer/producer.py
@@ -1,8 +1,17 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
 import pika
 import time
+import pymysql
 
-app=Flask(__name__)
+db=pymysql.connect(
+	host='host.docker.internal',
+	user='root', 
+	password='coleadonis23#J', 
+	db='ims'
+)
+cursor=db.cursor()
+
+app=Flask(__name__, static_folder="static", template_folder="templates")
 
 def connect_to_rabbitmq():
     connection=None
@@ -14,6 +23,7 @@ def connect_to_rabbitmq():
             channel.queue_declare(queue='item_creation_queue')
             channel.queue_declare(queue='order_processing_queue')
             channel.queue_declare(queue='stock_management_queue')
+            channel.queue_declare(queue='get_items_queue')
             print("Connected to rabbitmq")
         except pika.exceptions.AMQPConnectionError:
             print("Failed to connect, retrying in 5 sec")
@@ -22,9 +32,22 @@ def connect_to_rabbitmq():
 
 connection, channel=connect_to_rabbitmq()
 
+def get_items():
+    items=[]
+    method_frame, header_frame, body=channel.basic_get(queue="get_items_queue")
+    if method_frame:
+        items=eval(body.decode('utf-8'))
+        channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+    return items
+
 @app.route('/') 
 def home():
     return render_template('producer.html')
+
+@app.route('/getitems')
+def getitems():
+    items=get_items()
+    return render_template('viewitems.html', items=items)
 
 @app.route('/health_check', methods=['GET'])
 def health_check():
@@ -40,16 +63,16 @@ def create_item():
         'quantity': int(request.form["quantity"])
     }
     channel.basic_publish(exchange='', routing_key='item_creation_queue', body=str(data))
-    return 'Item creation request sent'
+    return redirect(url_for('getitems'))
 
-@app.route('/stock', methods=['PUT'])
+@app.route('/stock', methods=['POST'])
 def stock_update():
     data = {
         'item_id': int(request.form["item_id"]), 
         'quantity': int(request.form["quantity"])
     }
     channel.basic_publish(exchange='', routing_key='stock_management_queue', body=str(data))
-    return 'Stock update request sent'
+    return "stock update requested"
 
 @app.route('/orders', methods=['POST'])
 def order_process():
