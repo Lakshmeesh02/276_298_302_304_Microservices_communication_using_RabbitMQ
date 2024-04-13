42efbec64a64867c2842756a6fcde5cfd0c68535
Lakshmeesh Bhat <sherlywatson44@gmail.com>
2024-04-14 01:30:46 +0530

    renders updated itemlist to the producer after the update

--- consumer_3/stock_management.py
+++ consumer_3/stock_management.py
@@ -1,7 +1,7 @@
 import pika
 import pymysql
 import time
-
+ 
 db=pymysql.connect(
 	host='host.docker.internal', 
 	user='root', 
@@ -28,14 +28,28 @@ def connect_to_rabbitmq():
 connection, channel=connect_to_rabbitmq()
 
 def callback(ch, method, properties, body):
+	itemlist=[]
 	stock_data=eval(body)
 	print(f"Received stock update data: {stock_data}")
 	query="update items set quantity = %s where id = %s"
 	values=(stock_data["quantity"], stock_data["item_id"])
 	cursor.execute(query, values)
 	db.commit()
+	query="select * from items"
+	cursor.execute(query)
+	items=cursor.fetchall()
+	for item in items:
+		itemlist.append({
+			'id': item[0],
+			'name': item[1], 
+			'description': item[2],
+			'price': float(item[3]), 
+			'quantity': item[4]
+		})
+	channel.basic_publish(exchange='', routing_key="get_items_queue", body=str(itemlist))
+	channel.basic_ack(delivery_tag=method.delivery_tag)
 	print("Stock updated successfully")
 
-channel.basic_consume(queue='stock_management_queue', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(queue='stock_management_queue', on_message_callback=callback, auto_ack=False)
 print("Stock management queue ready, waiting for requests")
 channel.start_consuming()
